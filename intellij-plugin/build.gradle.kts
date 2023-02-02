1f6bfe41af3525fbe475dfe02c3c11837c87bd32
BoD <BoD@JRAF.org>
2023-02-02 10:50:58 +0100

    Add pluginsRepositories

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -37,6 +37,11 @@ intellij {
 
   // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
   plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+
+  pluginsRepositories {
+    maven("file://${System.getProperty("user.home")}/.m2/repository")
+    marketplace()
+  }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
